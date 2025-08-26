b16a5605783a3abccca7426b1d8829bbe2b4802d
realkoder <alexanderbtcc@gmail.com>
2025-08-26 21:17:20 +0200

    Added more test cases

--- lesson-01/spec/unit/app/roman_numeral_spec.rb
+++ lesson-01/spec/unit/app/roman_numeral_spec.rb
@@ -3,6 +3,9 @@ require_relative '../../../app/roman_numeral'
 RSpec.describe RomanNumeral do
   context 'positive testings' do
     context '#convert_number_to_roman' do
+    it 'converts 1 to I' do
+      expect(RomanNumeral.convert_number_to_roman(1)).to eq("I")
+    end
       it 'converts 4 to IV' do
         expect(RomanNumeral.convert_number_to_roman(4)).to eq("IV")
       end
@@ -15,12 +18,16 @@ RSpec.describe RomanNumeral do
         expect(RomanNumeral.convert_number_to_roman(10)).to eq("X")
       end
 
+      it 'converts 18 to XVIII' do
+        expect(RomanNumeral.convert_number_to_roman(18)).to eq("XVIII")
+      end
+
       it 'converts 20 to XX' do
         expect(RomanNumeral.convert_number_to_roman(20)).to eq("XX")
       end
 
-      it 'converts 18 to XVIII' do
-        expect(RomanNumeral.convert_number_to_roman(18)).to eq("XVIII")
+      it 'converts 3999 to MMMCMXCIX' do
+        expect(RomanNumeral.convert_number_to_roman(3999)).to eq("MMMCMXCIX")
       end
     end
   end
